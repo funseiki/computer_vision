3e419c036419c021d91f474bd843ec421809e129
Vinit Shah <elysiums.shade@gmail.com>
2013-02-21 19:18:35 -0600

    Load file and convert to gray

---
+++ homework2/homework2.m
@@ -0,0 +1,9 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Vinit Shah
+% CS 543 Homework 2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Get the image
+Original = imread('Images/Test/butterfly.jpg');
+GrayScaled = rgb2gray(Original);
+image = im2double(GrayScaled);
